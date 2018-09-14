081ac54eb9e424d4eea34c46c0166c8e121f26c4
Robin Tram <robin.tram@gmail.com>
2018-09-14 22:00:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,17 @@
 #write your code here
-def countdown_with_sleep
-  sleep 1 
-end
-
 def countdown (number)
   while number > 0 
    puts "#{number} SECOND(S)!"
    number -= 1
-   countdown_with_sleep
   end 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (number)
+  while number > 0 
+   puts "#{number} SECOND(S)!"
+   number -= 1
+   sleep 1 
+  end
   return "HAPPY NEW YEAR!"
 end
